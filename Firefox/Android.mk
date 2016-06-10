1d42c71e0e9343f858542c4f910297f37a484c7e
Caio <caiocanic@outlook.com>
2016-06-10 07:42:26 +0000

    Corrected Firefox Android.mk.

--- Firefox/Android.mk
+++ Firefox/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := Firefox	
+LOCAL_MODULE := Firefox
 LOCAL_SRC_FILES := fennec-46.0.multi.android-arm.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
