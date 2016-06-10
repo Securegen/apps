e910056a923f43b82001930c13c81b4b9600992c
Caio <caiocanic@outlook.com>
2016-06-10 09:03:00 +0000

    Corrected Orfox apk.

--- Orfox/Android.mk
+++ Orfox/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := Orfox
-LOCAL_SRC_FILES := FennecForTor_GSoC_prototype.apk
+LOCAL_SRC_FILES := info.guardianproject.orfox.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
