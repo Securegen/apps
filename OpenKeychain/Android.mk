8470d2ddd24341edec3fda3d362852a1dd7c74a4
Caio <caiocanic@outlook.com>
2016-08-09 19:40:21 +0000

    Updated apks.

--- OpenKeychain/Android.mk
+++ OpenKeychain/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := OpenKeychain
-LOCAL_SRC_FILES := org.sufficientlysecure.keychain_40100.apk
+LOCAL_SRC_FILES := org.sufficientlysecure.keychain_41010.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
--- Telegram/Android.mk
+++ Telegram/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := Telegram
-LOCAL_SRC_FILES := org.telegram.messenger_7673.apk
+LOCAL_SRC_FILES := org.telegram.messenger_8213.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
