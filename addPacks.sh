8f2226d2a14ae71e3415af6bb98eb636e4ff5dbe
Caio <caiocanic@outlook.com>
2016-10-19 19:23:48 +0000

    Minor modifications for better understanding the code.

--- addPacks.sh
+++ addPacks.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -e
 
-if [ -z ${cmHome+x} ]; then read -p "Enter the path to the build dir: " -e cmHome; fi
+if [ -z ${cmHome+x} ]; then
+	read -p "Enter the path to the build dir: " -e cmHome;
+fi
 
 cmPackages=$cmHome/packages/apps
 cmCommonFull=$cmHome/vendor/cm/config/common_full.mk
 
-if grep -Fxq "#Packages included by Securegen" $cmCommonFull
-then
+if grep -Fxq "#Packages included by Securegen" $cmCommonFull; then
 	echo "Packs already added."
 	echo "See our README for info on how to edit packs after inclusion."
 else
 	echo "Adding packages..."
-	cp -R */ $cmPackages    
+	cp -R */ $cmPackages
 	cat secGenPacks.txt >> $cmCommonFull
 	echo "Done."
 fi
